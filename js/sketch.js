e1bd44f0686cda65542682e9a9cd1eb088eb616d
jesusLuquinn <jesvicalm75@gmail.com>
2026-02-16 17:37:31 +0000

    l

---
+++ js/sketch.js
@@ -0,0 +1,41 @@
+function setup() {
+  createCanvas(800, 500);
+  background(180);
+}
+
+function draw() {
+  /*
+  stroke(0);
+  line(0,250,800,250);
+  line(400,0,400,800);
+  
+  fill(0,0,255);
+  noStroke();
+  circle(400,250,20);
+  
+  fill(0);
+  text("X: " + mouseX,10,20);
+  text("Y: " + mouseY,10,40);
+
+  fill(0, 100, 200);
+  circle(mouseX,mouseY,10);
+  */
+  
+  stroke(0);
+  line(30,20,185,20);
+  stroke(126);
+  line(185,20,185,175);
+  stroke(255);
+  line(185,175,30,175);
+  line(30,175,30,20);
+
+  stroke(0);
+  line(615,20,770,20);
+  stroke(126);
+  line(770,20,770,175);
+  stroke(255);
+  line(770,175,615,175);
+  line(615,175,615,20);
+
+  
+}
